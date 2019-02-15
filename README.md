126d81f50a9a536d1cbef777244329d296592d2c
254v0lkan <nzaro19@gmail.com>
2019-02-15 13:00:00 +0300

    complete

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Fake-Weather-Api-Starter
 
 This is the starter file for part on of my article on medium
+
+
+npm install or npm i
+
+nodemon index
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const express = require('express'); // import express
 const app = express(); // initialize express
+const fakeWeatherData = require('./data.js'); // import fake data
+const cors = require('cors'); // import cors package
+
+// Enable all CORS requests
+app.use(cors());
+
+
+// GET route
+app.get('/', function(req, res) {
+  res.send(fakeWeatherData[0]);
+})
+
+// GET route
+app.get('/weather', function (req, res) {
+
+// store the query string parameter in cityName variable
+  let cityName = req.query.city.toLowerCase();
+
+// Loop through our fake data array
+  for (let i = 0; i < fakeWeatherData.length; i++) {
+
+// if no city parameter exists
+    if (!cityName) {
+      return res.send({"status": "error", "message": "Please enter a city name"})
+    } else if (cityName == fakeWeatherData[i].city.toLowerCase()) {
+      return res.send(fakeWeatherData[i])
+    }
+  }
+
+// if city parameter isn't in our fake data set
+  res.send({"status": "error", "message": "This city isn't in our database"})
+
+});
 
 // Listen on port 3000
 app.listen(3000, function() {
